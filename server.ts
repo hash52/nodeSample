b76106a5535b931364e034ffedfb9097db9c0aea
上原拓也 <ueharatakuya@ueharatakuyanoMacBook-Air.local>
2024-08-01 06:18:47 +0900

    ルームを分けた通信を実装

--- server.ts
+++ server.ts
@@ -5,31 +5,49 @@ import path from "path";
 
 dotenv.config();
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server);
+const http = require( "http" );
+const server = http.createServer( app );
+const io = require( "socket.io" )( server );
 
 const PORT = process.env.PORT;
 
-app.use(express.static(path.join(__dirname)));
+app.use( express.static( path.join( __dirname ) ) );
 
-app.get("/", (request: Request, response: Response) => { 
-    response.status(200).sendFile(__dirname + "/index.html");
-  }); 
+app.get( "/", ( request: Request, response: Response ) =>
+{
+  response.status( 200 ).sendFile( __dirname + "/index.html" );
+} );
 
-  io.on("connection",(socket:Socket)=>{
-    console.log("ユーザーが接続しました")
+io.on( "connection", ( socket: Socket ) =>
+{
+  console.log( "ユーザーが接続しました" )
 
-    socket.on("chat message",(msg)=>{
-      console.log(`メッセージ:${msg}`);
-      //クライアントサイドにmessageを送り返す
-      io.emit("chat message",msg);
-    })
-  });
-  
-  server.listen(PORT, () => { 
-    console.log("Server running at PORT: ", PORT); 
-  }).on("error", (error:Error) => {
-    // エラーの処理
-    throw new Error(error.message);
-  });
+  socket.on( 'join room', ( room ) =>
+  {
+    socket.join( room );
+    console.log( `user joined room: ${ room }` );
+  } );
+
+  socket.on( "chat message", ( data ) =>
+  {
+    const { room, msg } = data;
+    console.log( `ルーム:${ room }｜｜メッセージ:${ msg }` );
+    //クライアントサイドにmessageを送り返す
+    io.to( room ).emit( "chat message", msg );
+  } )
+
+  socket.on( 'disconnect', () =>
+  {
+    console.log( "ユーザーが切断しました" );
+  } );
+
+} );
+
+server.listen( PORT, () =>
+{
+  console.log( "Server running at PORT: ", PORT );
+} ).on( "error", ( error: Error ) =>
+{
+  // エラーの処理
+  throw new Error( error.message );
+} );
