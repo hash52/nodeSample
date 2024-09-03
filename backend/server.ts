0672cc35cf70d4c134f1643f623a1105aa2f89ed
hashi.h <hashi.h@vpj.co.jp>
2024-09-03 16:27:53 +0900

    "/api/threads"でスレッド全件を返すAPIの実装

--- backend/server.ts
+++ backend/server.ts
@@ -2,52 +2,54 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { Socket } from "socket.io";
 import path from "path";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const app = express();
-const http = require( "http" );
-const server = http.createServer( app );
-const io = require( "socket.io" )( server );
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 const PORT = process.env.PORT;
 
-app.use( express.static( path.join( __dirname ) ) );
+const prisma = new PrismaClient();
 
-app.get( "/", ( request: Request, response: Response ) =>
-{
-  response.status( 200 ).sendFile( __dirname + "/index.html" );
-} );
+app.use(express.static(path.join(__dirname)));
 
-io.on( "connection", ( socket: Socket ) =>
-{
-  console.log( "ユーザーが接続しました" )
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).sendFile(__dirname + "/index.html");
+});
 
-  socket.on( 'join room', ( room ) =>
-  {
-    socket.join( room );
-    console.log( `user joined room: ${ room }` );
-  } );
+app.get("/api/threads", async (request: Request, response: Response) => {
+  const threads = await prisma.thread.findMany();
+  response.json(threads);
+});
 
-  socket.on( "chat message", ( data ) =>
-  {
+io.on("connection", (socket: Socket) => {
+  console.log("ユーザーが接続しました");
+
+  socket.on("join room", (room) => {
+    socket.join(room);
+    console.log(`user joined room: ${room}`);
+  });
+
+  socket.on("chat message", (data) => {
     const { room, msg } = data;
-    console.log( `ルーム:${ room }｜｜メッセージ:${ msg }` );
+    console.log(`ルーム:${room}｜｜メッセージ:${msg}`);
     //クライアントサイドにmessageを送り返す
-    io.to( room ).emit( "chat message", msg );
-  } )
+    io.to(room).emit("chat message", msg);
+  });
 
-  socket.on( 'disconnect', () =>
-  {
-    console.log( "ユーザーが切断しました" );
-  } );
+  socket.on("disconnect", () => {
+    console.log("ユーザーが切断しました");
+  });
+});
 
-} );
-
-server.listen( PORT, () =>
-{
-  console.log( "Server running at PORT: ", PORT );
-} ).on( "error", ( error: Error ) =>
-{
-  // エラーの処理
-  throw new Error( error.message );
-} );
+server
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error: Error) => {
+    // エラーの処理
+    throw new Error(error.message);
+  });
